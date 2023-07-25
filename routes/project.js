3bcfa95d4b4ee1954e43ea5e6ed0f5f8ea050906
Sunaa <sunaa@sunaarisu.de>
2023-07-25 09:21:55 +0200

    new: authAndGetData middleware to verify jwt tokens inserted aagd in project create route

--- routes/project.js
+++ routes/project.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const aagd = require('../middleware/authAndGetData');
 
 const ProjectController = require('../controllers/project');
 
-router.post("/create", ProjectController.project_create);
+router.post("/create", aagd, ProjectController.project_create);
 
 module.exports = router;
