db409b08e98926e0bdcf9f3c46e368dffbedbd05
Sunaa <sunaa@sunaarisu.de>
2023-08-03 12:46:24 +0200

    new start command

--- routes/project.js
+++ routes/project.js
@@ -6,8 +6,8 @@ const ProjectController = require('../controllers/project');
 
 router.post("/create", aagd, ProjectController.project_create);
 
-router.get("/get", aagd, ProjectController.project_get_all);
+router.get("/get", ProjectController.project_get_all);
 
-router.post("/get-specific", aagd, ProjectController.project_get_specific);
+router.post("/get-specific", ProjectController.project_get_specific);
 
 module.exports = router;
