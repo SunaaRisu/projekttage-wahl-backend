201a8aaeb1a203ec0604e8aaa09bc01675165bff
Sunaa <sunaa@sunaarisu.de>
2023-08-03 17:44:26 +0200

    cors

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 
 app.use(
     cors({ 
-        origin: 'http://localhost:5173', 
+        origin: '*', 
         methods: ['GET', 'POST'], 
         credentials :  false
     })
